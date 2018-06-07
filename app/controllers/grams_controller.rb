42f1b63d6109fbb2cc27dc7ec9a03aa8495ddd44
fawezholanda <fawezholanda@hotmail.com>
2018-06-07 21:54:46 +0000

    Lesson 13 in progress

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -25,6 +25,14 @@ class GramsController < ApplicationController
     end
   end
 
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      render plain: 'Not Found :(', status: :not_found
+    end
+  end
+
+
 private
 
   def gram_params
