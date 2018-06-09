b1820a6a939289b6293912f24e887980d17e4dbb
fawezholanda <fawezholanda@hotmail.com>
2018-06-09 15:25:43 +0000

    Lesson 15 finished

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,9 +1,12 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def destroy
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    #if @gram.user != current_user
+    #  return render plain: 'Forbidden :(', status: :forbidden
+    return render_not_found(:forbidden) if @gram.user != current_user
     @gram.destroy
     redirect_to root_path
   end
@@ -11,6 +14,9 @@ class GramsController < ApplicationController
   def update
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    #if @gram.user != current_user
+    #  return render plain: 'Forbidden :(', status: :forbidden
+    return render_not_found(:forbidden) if @gram.user != current_user
 
     @gram.update_attributes(gram_params)
     if @gram.valid?
@@ -47,17 +53,20 @@ class GramsController < ApplicationController
   def edit
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+    #if @gram.user != current_user
+    #  render plain: 'Forbidden :(', status: :forbidden
   end
 
 
-private
+  private
 
   def gram_params
     params.require(:gram).permit(:message)
   end
 
-  def render_not_found
-    render plain: 'Not Found :(', status: :not_found
+  def render_not_found(status=:not_found)
+    render plain: '#{status.to_s.titleize} :(', status: status
   end  
 
 end
